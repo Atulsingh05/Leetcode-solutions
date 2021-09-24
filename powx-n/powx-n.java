18392bb46ab6f079e09c1162c92bcab5a9f3f6f9
Atulsingh05 <73822911+Atulsingh05@users.noreply.github.com>
2021-09-24 17:00:36 +0530

    Update powx-n.java

--- powx-n/powx-n.java
+++ powx-n/powx-n.java
@@ -1,8 +1,23 @@
-class Solution 
-{
-    public double myPow(double x, int n) 
-    {
-        double ans =Math.pow(x,n);
+class Solution {
+    public double myPow(double x, int n) {
+      
+        double ans=1.0;
+        long nn=n;
+        if(nn<0) nn=-1*nn;//agar nn negative h to use positive bana do.
+        while(nn>0)
+        {
+            if(nn%2==1)//if nn  is odd
+            {
+                ans=ans*x;
+                nn=nn-1;
+            }
+            else//if nn is even.
+            {
+                x=x*x;
+                nn=nn/2;
+            }
+        }
+        if(n<0) ans=(double)(1.0) / (double)(ans);//return 1/x^n;
         return ans;
     }
 }
