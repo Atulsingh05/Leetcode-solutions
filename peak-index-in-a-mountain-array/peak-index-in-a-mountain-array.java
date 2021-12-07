27731dc3d0a912696bf4cd7be49770e66af3958d
Atulsingh05 <73822911+Atulsingh05@users.noreply.github.com>
2021-12-07 21:09:25 +0530

    Time: 1 ms (9.08%), Space: 43.3 MB (22.95%) - LeetHub

---
+++ peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.java
@@ -0,0 +1,21 @@
+class Solution 
+{
+    public int peakIndexInMountainArray(int[] arr)
+    {
+        int res=0;
+        int max=arr[0];
+       
+      for(int i=1;i<arr.length-1;i++)
+      {
+           
+          if(arr[i]>max)
+          {
+              max=arr[i];
+              res=i;
+          }
+      }
+        return res;
+    }
+}
+
+      
